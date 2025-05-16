27a80bb203a816648cbb5b3ef80e58f7376fc346
Nona Pareulidze <nona.pareulidze1@gmail.com>
2025-05-17 00:30:05 +0400

    note

--- lin1/note.js
+++ lin1/note.js
@@ -19,3 +19,19 @@ document.querySelector(".note").addEventListener("input", function() {
     this.style.height = "auto";
     this.style.height = this.scrollHeight + "px";
 });
+
+document.querySelector(".them").addEventListener("click", light);
+function light(){
+    document.body.classList.toggle("light");
+    document.querySelector(".note").classList.toggle("light");
+    document.querySelectorAll("button").forEach(b=>{b.classList.toggle("light");});
+    document.querySelector(".header").classList.toggle("borderbpttom");
+    localStorage.setItem("them",document.body.classList.contains("light")?"light":"");}
+
+document.addEventListener("DOMContentLoaded",function(){
+    let t=localStorage.getItem("them");
+    if(t==="light"){
+light();
+
+    }
+})
